f126aaed9fedc0a04569b5e17a407c13f53f9979
DELL <marek.repisky55313@gmail.com>
2025-02-19 00:25:41 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button>Order pizza</button><br />
+    <?php
+        $a = "asd";
+        echo "I love 
+        pizza <br />";
+        echo phpversion() . "<br />";
+        print get_defined_vars()[1];
+
+        // Comment
+    ?>
+</body>
+</html>
